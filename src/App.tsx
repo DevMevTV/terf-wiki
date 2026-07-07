3cb61e0af4a2341a51753086ff68f793b0789e19
DevMevTV <devidfitz533@gmail.com>
2026-07-07 22:12:39 +0200

    switched to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from "./pages/Home";
 import Item from "./pages/Item";
 
 function App() {
   return (
-    <BrowserRouter basename="/terf-wiki">
+    <HashRouter basename="/terf-wiki">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/item/:id" element={<Item />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
