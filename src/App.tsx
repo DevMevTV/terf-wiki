ce0788ad6a87a0a6ae89891970a0687b175c6edd
DevMevTV <devidfitz533@gmail.com>
2026-07-07 22:16:00 +0200

    help

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Item from "./pages/Item";
 
 function App() {
   return (
-    <HashRouter basename="/terf-wiki">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/item/:id" element={<Item />} />
